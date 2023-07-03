a715fe92525c11785ae84b518b7b5b7cf0c2a294
dimasfahmi <dimasfahmi998@gmail.com>
2023-07-03 10:41:13 +0700

    Fahmi

--- tugas/login.py
+++ tugas/login.py
@@ -10,26 +10,26 @@ class TestLogin(unittest.TestCase):  # test scenario
     def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def test_success_login(self):  # test cases 1 try this one change
-        baseUrl = "https://myskill.id/"
-        driver = self.browser
-        driver.implicitly_wait(10)
-        driver.get(baseUrl)
-        driver.find_element(
-            By.CLASS_NAME,
-            "MuiTouchRipple-root mui-style-w0pj6f",
-        ).click()
-        driver.find_element(
-            By.XPATH,
-            "/html//div[@id='__next']/div[@class='MuiBox-root mui-style-0']/div[@class='mui-style-v82cpl']//form//input[@name='email']",
-        ).send_keys("dimasfahmi998@gmail.com")
-        driver.find_element(
-            By.XPATH,
-            "/html//div[@id='__next']/div[@class='MuiBox-root mui-style-0']/div[@class='mui-style-v82cpl']//form//input[@name='password']",
-        ).send_keys("dimasfahmi1")
-        driver.find_element(
-            By.XPATH, "/html//div[@id='__next']//form/button[@type='submit']"
-        ).click()
+    # def test_success_login(self):  # test cases 1 try this one change
+    #     baseUrl = "https://myskill.id/"
+    #     driver = self.browser
+    #     driver.implicitly_wait(10)
+    #     driver.get(baseUrl)
+    #     driver.find_element(
+    #         By.CLASS_NAME,
+    #         "MuiTouchRipple-root mui-style-w0pj6f",
+    #     ).click()
+    #     driver.find_element(
+    #         By.XPATH,
+    #         "/html//div[@id='__next']/div[@class='MuiBox-root mui-style-0']/div[@class='mui-style-v82cpl']//form//input[@name='email']",
+    #     ).send_keys("dimasfahmi998@gmail.com")
+    #     driver.find_element(
+    #         By.XPATH,
+    #         "/html//div[@id='__next']/div[@class='MuiBox-root mui-style-0']/div[@class='mui-style-v82cpl']//form//input[@name='password']",
+    #     ).send_keys("dimasfahmi1")
+    #     driver.find_element(
+    #         By.XPATH, "/html//div[@id='__next']//form/button[@type='submit']"
+    #     ).click()
 
     def test_success_orange_login(self):  # test case 2
         baseUrl = "https://opensource-demo.orangehrmlive.com/"
@@ -43,16 +43,16 @@ class TestLogin(unittest.TestCase):  # test scenario
             "//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']",
         ).click()
 
-    def test_field_orange_login(self):  # test case 3
-        baseUrl = "https://opensource-demo.orangehrmlive.com/"
-        driver = self.browser
-        driver.implicitly_wait(10)
-        driver.get(baseUrl)
-        driver.find_element(By.NAME, "username").send_keys("admin")
-        driver.find_element(
-            By.XPATH,
-            "//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']",
-        ).click()
+    # def test_field_orange_login(self):  # test case 3
+    #     baseUrl = "https://opensource-demo.orangehrmlive.com/"
+    #     driver = self.browser
+    #     driver.implicitly_wait(10)
+    #     driver.get(baseUrl)
+    #     driver.find_element(By.NAME, "username").send_keys("admin")
+    #     driver.find_element(
+    #         By.XPATH,
+    #         "//div[@id='app']/div[@class='orangehrm-login-layout']/div[@class='orangehrm-login-layout-blob']//form[@action='/web/index.php/auth/validate']/div[3]/button[@type='submit']",
+    #     ).click()
 
 
 if __name__ == "__main__":
